2f0074e44b16b4f38505db5513d1bb71ca5fede8
Vladimir Kononov <vladimirkon@gmail.com>
2017-09-18 18:40:39 -0700

    Update pom.xml, AutoBasics.java

--- src/main/java/com/sqa/vk/helpers/AutoBasics.java
+++ src/main/java/com/sqa/vk/helpers/AutoBasics.java
@@ -2,7 +2,9 @@ package com.sqa.vk.helpers;
 
 import java.io.*;
 
+import org.apache.commons.io.*;
 import org.apache.log4j.*;
+import org.openqa.selenium.*;
 
 public class AutoBasics {
 
@@ -18,5 +20,26 @@ public class AutoBasics {
 			logger.warn("Can not load config properties file can not be read: " + fileName);
 		}
 		return props.getProperty(propName);
+	}
+
+	public static boolean isElementPresent(WebDriver driver, By by, Logger logger) {
+		try {
+			WebElement element = driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			logger.warn("Element was not found: " + by);
+			return false;
+		}
+	}
+
+	public static boolean takeScreenshot(String fileLocation, String fileName, WebDriver driver, Logger logger) {
+		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(srcFile, new File(fileLocation + "/" + fileName + ".png"));
+			return true;
+		} catch (IOException e) {
+			logger.warn("Screenshot " + fileName + " was not captured to disk correctly.");
+			return false;
+		}
 	}
 }
